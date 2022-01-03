099fb4030b598b04084a1688861945c100e4bb30
mariodavid <mario@die-davids.de>
2022-01-03 11:00:21 +0100

    changed code to perform a test case with system authentication

--- jmix-addon-template/src/test/java/io/jmix/addon/template/AddonTest.java
+++ jmix-addon-template/src/test/java/io/jmix/addon/template/AddonTest.java
@@ -3,6 +3,8 @@ package io.jmix.addon.template;
 import io.jmix.addon.template.test_support.entity.Foo;
 import io.jmix.core.DataManager;
 import io.jmix.core.Id;
+import io.jmix.core.UnconstrainedDataManager;
+import io.jmix.core.security.SystemAuthenticator;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,7 +15,9 @@ import static org.junit.jupiter.api.Assertions.*;
 class AddonTest {
 
 	@Autowired
-	DataManager dataManager;
+	UnconstrainedDataManager dataManager;
+	@Autowired
+	SystemAuthenticator systemAuthenticator;
 
 	@Test
 	void contextLoads() {
@@ -21,13 +25,20 @@ class AddonTest {
 
 	@Test
 	void testFoo() {
-		Foo foo = dataManager.create(Foo.class);
-		foo.setName("abc");
 
-		Foo foo1 = dataManager.save(foo);
-		assertEquals(foo, foo1);
+		systemAuthenticator.withSystem(() ->  {
+			Foo foo = dataManager.create(Foo.class);
+			foo.setName("abc");
 
-		Foo foo2 = dataManager.load(Id.of(foo)).one();
-		assertEquals(foo, foo2);
+			Foo foo1 = dataManager.save(foo);
+			assertEquals(foo, foo1);
+
+			Foo foo2 = dataManager.load(Id.of(foo)).one();
+			assertEquals(foo, foo2);
+
+			return "Done";
+		});
+
+
 	}
 }
--- jmix-addon-template/src/test/java/io/jmix/addon/template/AddonTestConfiguration.java
+++ jmix-addon-template/src/test/java/io/jmix/addon/template/AddonTestConfiguration.java
@@ -1,6 +1,8 @@
 package io.jmix.addon.template;
 
 import io.jmix.core.annotation.JmixModule;
+import io.jmix.core.security.InMemoryUserRepository;
+import io.jmix.core.security.UserRepository;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
@@ -16,6 +18,12 @@ import javax.sql.DataSource;
 @Import(AddonConfiguration.class)
 @JmixModule(dependsOn = AddonConfiguration.class)
 public class AddonTestConfiguration {
+
+
+    @Bean
+    public UserRepository userRepository() {
+        return new InMemoryUserRepository();
+    }
 
     @Bean
     @Primary
