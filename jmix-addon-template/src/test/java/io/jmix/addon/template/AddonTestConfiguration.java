88abcadc23b25d560af30be568539abf9e08eacc
mariodavid <mario@die-davids.de>
2022-01-11 05:12:34 +0100

    adding SecurityDataConfiguration also to AddonTestConfiguration

--- jmix-addon-template/src/test/java/io/jmix/addon/template/AddonTestConfiguration.java
+++ jmix-addon-template/src/test/java/io/jmix/addon/template/AddonTestConfiguration.java
@@ -3,8 +3,11 @@ package io.jmix.addon.template;
 import io.jmix.core.annotation.JmixModule;
 import io.jmix.core.security.InMemoryUserRepository;
 import io.jmix.core.security.UserRepository;
+import io.jmix.eclipselink.EclipselinkConfiguration;
+import io.jmix.ui.UiConfiguration;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.SecurityDataConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Primary;
@@ -16,7 +19,7 @@ import javax.sql.DataSource;
 @SpringBootConfiguration
 @EnableAutoConfiguration
 @Import(AddonConfiguration.class)
-@JmixModule(dependsOn = AddonConfiguration.class)
+@JmixModule(dependsOn = {AddonConfiguration.class, EclipselinkConfiguration.class, UiConfiguration.class, SecurityDataConfiguration.class})
 public class AddonTestConfiguration {
 
 
